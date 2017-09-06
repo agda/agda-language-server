6dde4150541606d76c426d5bfa5c29cb95cfd90c
Lai Ting Yan <banacorn@gmail.com>
2017-09-06 22:14:34 +0800

    get things moving

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@ module Main (main) where
 
 import System.Exit
 import System.Process
+import System.IO
 import System.Info (os)
 import System.Console.CmdArgs
 
@@ -12,7 +13,7 @@ import System.Console.CmdArgs
 
 data Args = Args {
         search :: String    -- the name of Agda executable to search with
-    ,   path :: FilePath    -- the path of Agda executable
+    ,   path :: Maybe FilePath    -- the path of Agda executable
     }   deriving (Show, Data, Typeable)
 
 defaultArgs :: Args
@@ -20,7 +21,7 @@ defaultArgs = Args {
         search = "agda"
             &= typ "STRING"
             &= help "the name of Agda executable to search with (default: \"agda\")"
-    ,   path = def
+    ,   path = Nothing
             &= typFile
             &= help "the path of Agda executable"
     }
@@ -30,15 +31,46 @@ defaultArgs = Args {
 main :: IO ()
 main = do
     args <- cmdArgs defaultArgs
-    print args
+    p <- case path args of
+        Nothing -> which (search args)  -- no path specified
+        Just p -> return (Just p)
+    case p of
+        Nothing -> putStrLn "unable to find Agda, please supply a different path to Agda"
+        Just p -> spawnAgda p
+            -- putStr ">> "
+            -- line <- getLine
+            -- print $ " " ++ line
     return ()
 
+spawnAgda :: FilePath -> IO ()
+spawnAgda path = do
+    (Just hin, _, _, procHandle) <- createProcess $ (proc path ["--interaction"])
+        {   std_in = CreatePipe
+        }
+    hSetBuffering stdout NoBuffering
+    hSetBuffering hin LineBuffering
 
-which :: FilePath -> IO (Maybe String)
-which path = do
+    hPutStrLn hin (load path0)
+
+    loop hin
+    exitCode <- getProcessExitCode procHandle
+    print exitCode
+
+    where   loop hin = do
+                getLine >>= hPutStrLn hin
+                loop hin
+
+            path0 = "\"/Users/banacorn/agda/test/A.agda\""
+            load path =
+                "IOTCM " ++ path0 ++ " NonInteractive Indirect ( Cmd_load " ++ path0 ++ " [])"
+
+test = spawnAgda "/Users/banacorn/.local/bin/bgda"
+
+which :: String -> IO (Maybe String)
+which name = do
     (code, stdout, stderr) <- case os of
-        "mingw32" -> readProcessWithExitCode "where.exe" [path] "" -- not tested on Windows yet
-        _         -> readProcessWithExitCode "which"     [path] ""
+        "mingw32" -> readProcessWithExitCode "where.exe" [name] "" -- not tested on Windows yet
+        _         -> readProcessWithExitCode "which"     [name] ""
 
     case code of
         ExitSuccess   -> return (Just $ sanitize stdout)
