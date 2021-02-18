bcec3a0315645ca3f0459489aa71a514a4b34b54
Ting-Gian LUA <banacorn@gmail.com>
2021-02-18 16:58:20 +0800

    [ change ] Beef SizedChan up

--- src/Control/Concurrent/SizedChan.hs
+++ src/Control/Concurrent/SizedChan.hs
@@ -1,15 +1,16 @@
 -- | Chan with size
-module Control.Concurrent.SizedChan where
+module Control.Concurrent.SizedChan (SizedChan, newSizedChan, writeSizedChan, readSizedChan, tryReadSizedChan, peekSizedChan, tryPeekSizedChan) where
 
-import Control.Concurrent.Chan (Chan, newChan, readChan, writeChan)
-import Data.IORef (IORef, modifyIORef', newIORef, readIORef)
-
+import Control.Concurrent.Chan
+import Data.IORef
 
 data SizedChan a = SizedChan
   { -- | The channel
     chan :: Chan a,
     -- | Its size
-    size :: IORef Int
+    size :: IORef Int,
+    -- | Peeked payload
+    peeked :: IORef (Maybe a)
   }
 
 -- | Build and returns a new instance of 'SizedChan'.
@@ -18,38 +19,68 @@ newSizedChan =
   SizedChan
     <$> newChan
     <*> newIORef 0
+    <*> newIORef Nothing
 
 -- | Write a value to a 'SizedChan'.
 writeSizedChan :: SizedChan a -> a -> IO ()
-writeSizedChan (SizedChan chan sizeIORef) val = do
+writeSizedChan (SizedChan chan sizeIORef peeked) val = do
   writeChan chan val
   modifyIORef' sizeIORef succ
 
 -- | Read the next value from the 'SizedChan'. Blocks when the channel is empty.
 readSizedChan :: SizedChan a -> IO a
-readSizedChan (SizedChan chan sizeIORef) = do
-  val <- readChan chan
-  modifyIORef' sizeIORef pred
-  return val
+readSizedChan (SizedChan chan sizeIORef peekedIORef) = do
+  peeked <- readIORef peekedIORef
+  case peeked of
+    -- return and remove the peeked value
+    Just val -> do
+      writeIORef peekedIORef Nothing
+      modifyIORef' sizeIORef pred
+      return val
+    -- else read from the channel
+    Nothing -> do
+      val <- readChan chan
+      modifyIORef' sizeIORef pred
+      return val
 
--- -- | Peek the next value from the 'SizedChan' without removing it. Blocks when the channel is empty.
--- peekSizedChan :: SizedChan a -> IO a
--- peekSizedChan (SizedChan chan sizeIORef) = do
---   val <- tryReadSizedChan chan
---   modifyIORef' sizeIORef pred
---   return val
+-- | A version of `readSizedChan` which does not block. Instead it returns Nothing if no value is available.
+tryReadSizedChan :: SizedChan a -> IO (Maybe a)
+tryReadSizedChan (SizedChan chan sizeIORef peekedIORef) = do
+  peeked <- readIORef peekedIORef
+  case peeked of
+    -- return and remove the peeked value
+    Just val -> do
+      writeIORef peekedIORef Nothing
+      modifyIORef' sizeIORef pred
+      return $ Just val
+    -- check the size before reading from the channel, to prevent blocking
+    Nothing -> do
+      size <- readIORef sizeIORef
+      if size == 0
+        then return Nothing
+        else do
+          val <- readChan chan
+          modifyIORef' sizeIORef pred
+          return $ Just val
 
-measureSizedChan :: SizedChan a -> IO Int 
-measureSizedChan (SizedChan _ sizeIORef) = readIORef sizeIORef
+-- | Peek the next value from the 'SizedChan' without removing it. Blocks when the channel is empty.
+peekSizedChan :: SizedChan a -> IO a
+peekSizedChan (SizedChan chan sizeIORef peekedIORef) = do
+  peeked <- readIORef peekedIORef
+  case peeked of
+    -- return the peeked value
+    Just val -> return val
+    -- read from the channel instead
+    Nothing -> readChan chan
+
+-- | A version of `peekSizedChan` which does not block. Instead it returns Nothing if no value is available.
+tryPeekSizedChan :: SizedChan a -> IO (Maybe a)
+tryPeekSizedChan (SizedChan chan sizeIORef peekedIORef) = readIORef peekedIORef
+
+measureSizedChan :: SizedChan a -> IO Int
+measureSizedChan (SizedChan _ sizeIORef _) = readIORef sizeIORef
 
 isEmptySizedChan :: SizedChan a -> IO Bool
-isEmptySizedChan chan = do 
+isEmptySizedChan chan = do
   size <- measureSizedChan chan
   return $ size == 0
-
-tryReadSizedChan :: SizedChan a -> IO (Maybe a)
-tryReadSizedChan chan = do 
-  isEmpty <- isEmptySizedChan chan 
-  if isEmpty 
-    then return Nothing 
-    else Just <$> readSizedChan chan
