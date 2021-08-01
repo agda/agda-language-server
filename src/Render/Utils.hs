b07409772487646262452ceb5a8cad8a7e93b736
Liang-Ting Chen <liang.ting.chen.tw@gmail.com>
2021-08-01 16:44:00 +0800

    [ refactor ] Use explicit imports

--- src/Render/Utils.hs
+++ src/Render/Utils.hs
@@ -1,9 +1,11 @@
 module Render.Utils where
 
+import Agda.Utils.Time ( CPUTime )
+import Agda.Utils.Pretty (pretty)
+
 import Render.Class
 import Render.RichText
-import Agda.Utils.Time
-import Agda.Utils.Pretty (pretty)
+
 
 instance Render CPUTime where
   render = text . show . pretty
