0cdb6fa316513ce605a22e933bd91542ee4132c2
Andreas Abel <andreas.abel@ifi.lmu.de>
2023-11-20 16:49:39 +0800

    Bump actions versions in CI, cosmetic changes

--- .github/workflows/mac-bundle-dylib.sh
+++ .github/workflows/mac-bundle-dylib.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # Bundle icu4c DLLs
 
-# see if icu4c has been installed 
+# see if icu4c has been installed
 if [ "$(brew list | grep icu4c)" = "" ]
     then
-        echo "installing icu4c" 
+        echo "installing icu4c"
         brew install icu4c
 fi
 
@@ -14,12 +14,12 @@ dylib_dir=$(dirname "$(brew list icu4c | grep icuuc.dylib)")
 # find the path of "als"
 executable=$(find "$(stack path --local-install-root)"/bin -name "als")
 
-# remove the old dylib, and make a new one 
+# remove the old dylib, and make a new one
 rm -rf dylib
-mkdir dylib 
+mkdir dylib
 
 ################################################################################
-# icuuc 
+# icuuc
 ################################################################################
 
 icuuc_id=$(otool -L "$executable" | grep icuuc | awk '{print $1}')
@@ -33,7 +33,7 @@ icuuc_id_new=@loader_path/dylib/$icuuc_id_basename
 cp "$icuuc_path" "$icuuc_path_new"
 
 # change icuuc's ID referenced by ALS
-install_name_tool -change "$icuuc_id" "$icuuc_id_new" "$executable" 
+install_name_tool -change "$icuuc_id" "$icuuc_id_new" "$executable"
 
 echo "icuuc referenced by ALS"
 echo "    old ID  : $icuuc_id"
@@ -42,7 +42,7 @@ echo "    old path: $icuuc_path"
 echo "    new path: $icuuc_path_new"
 
 ################################################################################
-# icui18n 
+# icui18n
 ################################################################################
 
 icui18n_id=$(otool -L "$executable" | grep icui18n | awk '{print $1}')
@@ -56,7 +56,7 @@ icui18n_id_new=@loader_path/dylib/$icui18n_id_basename
 cp "$icui18n_path" "$icui18n_path_new"
 
 # change icui18n's ID referenced by ALS
-install_name_tool -change "$icui18n_id" "$icui18n_id_new" "$executable" 
+install_name_tool -change "$icui18n_id" "$icui18n_id_new" "$executable"
 
 echo "icui18n referenced by ALS"
 echo "    old ID  : $icui18n_id"
@@ -65,7 +65,7 @@ echo "    old path: $icui18n_path"
 echo "    new path: $icui18n_path_new"
 
 ################################################################################
-# icudata 
+# icudata
 ################################################################################
 
 # otool -L "$icui18n_id" | grep icudata | awk '{print $1}'
