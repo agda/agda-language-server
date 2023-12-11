9d2dc14af0749bca7ea649efdcafd50d8dbe7b27
Ting-gian LUA <banacorn@gmail.com>
2023-12-11 22:44:52 +0800

    [ fix ] Set locale of stdin/stdout to utf8

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,18 @@
 module Main where
 
-import           Options
-import           Server                         ( run )
-import           System.Console.GetOpt
-import           System.Environment             ( getArgs )
-import           Text.Read                      ( readMaybe )
+import Options
+import Server (run)
+import System.Console.GetOpt
+import System.Environment (getArgs)
+import System.IO
+import Text.Read (readMaybe)
 
 main :: IO ()
 main = do
+  -- set locale to UTF-8
+  -- https://github.com/agda/agda-language-server/issues/24
+  hSetEncoding stdout utf8
+  hSetEncoding stdin utf8
   options <- getOptionsFromArgv
   if optHelp options
     then putStrLn usageMessage
