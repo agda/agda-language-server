a1a7d365006704665f82b3cb926ae988ff2cde51
Ting-Gian LUA <banacorn@gmail.com>
2024-12-04 10:56:06 +0800

    [ ci ] Unify the CI workflow for testing on all platforms

--- test/Test/LSP.hs
+++ test/Test/LSP.hs
@@ -1,40 +1,42 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Test.LSP (tests) where
 
-import Control.Applicative.Combinators
+import Agda
 import Control.Monad.IO.Class
+import qualified Data.Aeson as JSON
 import Language.LSP.Protocol.Message
-import Language.LSP.Protocol.Types
 import Language.LSP.Test
+import Switchboard (agdaCustomMethod)
 import Test.Tasty
 import Test.Tasty.HUnit
-import qualified Data.Aeson                    as JSON
-import Switchboard ( agdaCustomMethod )
-import Agda
 
 tests :: FilePath -> TestTree
-tests alsPath = 
+tests alsPath =
   testGroup
     "LSP"
     [ testCase "load" (demo alsPath)
     ]
 
 demo :: FilePath -> IO ()
-demo alsPath = 
+demo alsPath = do
+  putStrLn $ "Running LSP tests on the server with the following path to the als executable: " ++ alsPath
   runSession alsPath fullLatestClientCaps "test/data/" $ do
-  doc <- openDoc "A.agda" "agda"
+    doc <- openDoc "A.agda" "agda"
 
-  -- Use your favourite favourite combinators.
-  -- skipManyTill loggingNotification (count 1 publishDiagnosticsNotification)
+    -- Use your favourite favourite combinators.
+    -- skipManyTill loggingNotification (count 1 publishDiagnosticsNotification)
 
-  testCustomMethod "IOTCM \"test/data/A.agdaa\" NonInteractive Direct( Cmd_load \"test/data/A.agda\" [] )"
+    testCustomMethod "IOTCM \"test/data/A.agdaa\" NonInteractive Direct( Cmd_load \"test/data/A.agda\" [] )"
 
-  -- Or use one of the helper functions
-  -- getDocumentSymbols doc >>= liftIO . print
+-- Or use one of the helper functions
+-- getDocumentSymbols doc >>= liftIO . print
 
 -- | Sends a custom method request to the server and expects a response of `CmdRes Nothing`
 testCustomMethod :: String -> Session ()
-testCustomMethod cmd = do 
-  TResponseMessage _ _ rsp <- request agdaCustomMethod $ 
-    JSON.toJSON $ CmdReq cmd
+testCustomMethod cmd = do
+  TResponseMessage _ _ rsp <-
+    request agdaCustomMethod $
+      JSON.toJSON $
+        CmdReq cmd
   liftIO $ rsp @?= Right (JSON.toJSON (CmdRes Nothing))
